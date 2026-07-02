58167a3d4d6510cb5fb37e72b53b8d26b486e0a8
MagneticNeedle <solankivibhakar82@gmail.com>
2026-07-02 21:55:37 +0530

    chore: convet to monorepo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import Uppy from '@uppy/core';
+import Tus from '@uppy/tus';
+import Dashboard from '@uppy/react/dashboard';
+
+import '@uppy/dashboard/css/style.css';
+import './index.css';
+
+export function App() {
+	const [uppy] = useState(() =>
+		new Uppy().use(Tus, {
+			endpoint: 'http://localhost:8080/files/',
+		}),
+	);
+
+	return (
+		<main>
+			<Dashboard
+				uppy={uppy}
+				theme="dark"
+				width="100%"
+				height={550}
+				showProgressDetails={true}
+				singleFileFullScreen={false}
+			/>
+		</main>
+	);
+}
+
+export default App;
